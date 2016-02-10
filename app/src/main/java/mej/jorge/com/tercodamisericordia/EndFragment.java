2d7afc4a59ba5ecc3440be3feebd5e470e75faed
Jorge Ribeiro <joorgemelo@gmail.com>
2016-02-09 18:56:56 -0600

    Correção API e adição de oração final ao terço
    Min API 8 e oremos na oração final do terço

--- app/src/main/java/mej/jorge/com/tercodamisericordia/EndFragment.java
+++ app/src/main/java/mej/jorge/com/tercodamisericordia/EndFragment.java
@@ -15,6 +15,27 @@ public class EndFragment extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_end, container, false);
+        View view;
+
+        view = inflater.inflate(R.layout.fragment_end, container, false);
+
+        View buttonOremos = view.findViewById(R.id.botaoOremos);
+        buttonOremos.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AlertDialog.Builder builder = new AlertDialog.Builder(getActivity())
+                        .setTitle("Oremos")
+                        .setMessage(R.string.oremosFinal)
+                        .setPositiveButton("Amém", new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int which) {
+                                // Do something else
+                            }
+                        });
+                AlertDialog alert = builder.create();
+                alert.show();
+            }
+        });
+        
+        return view;
     }
 }
