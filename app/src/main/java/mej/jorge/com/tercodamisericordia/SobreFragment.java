46f59d0369b24ad546d211f677d8919dd53a6af0
Jorge Ribeiro <joorgemelo@gmail.com>
2016-02-10 09:21:39 -0600

    Atualização de botão
    Silencioso e escondido em versões mais antigas

--- app/src/main/java/mej/jorge/com/tercodamisericordia/SobreFragment.java
+++ app/src/main/java/mej/jorge/com/tercodamisericordia/SobreFragment.java
@@ -2,6 +2,7 @@ package mej.jorge.com.tercodamisericordia;
 
 import android.app.AlertDialog;
 import android.content.DialogInterface;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
@@ -18,26 +19,31 @@ public class SobreFragment extends Fragment {
         View view = inflater.inflate(R.layout.fragment_sobre, container, false);
 
         View sammyraBtn = view.findViewById(R.id.sammyra);
-        sammyraBtn.setOnClickListener(new View.OnClickListener() {
-            int countClick = 0;
-            @Override
-            public void onClick(View v) {
-                countClick++;
-                if(countClick == 18) {
-                    countClick = 0;
-                    AlertDialog.Builder builder = new AlertDialog.Builder(getActivity())
-                            .setTitle("Sammyra")
-                            .setMessage(R.string.texto_sammyra)
-                            .setPositiveButton("♥", new DialogInterface.OnClickListener() {
-                                public void onClick(DialogInterface dialog, int which) {
-                                    // Do something else
-                                }
-                            });
-                    AlertDialog alert = builder.create();
-                    alert.show();
+        int currentApi = Build.VERSION.SDK_INT;
+        if(currentApi >= Build.VERSION_CODES.HONEYCOMB) {
+            sammyraBtn.setOnClickListener(new View.OnClickListener() {
+                int countClick = 0;
+                @Override
+                public void onClick(View v) {
+                    countClick++;
+                    if(countClick == 18) {
+                        countClick = 0;
+                        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity())
+                                .setTitle("Sammyra")
+                                .setMessage(R.string.texto_sammyra)
+                                .setPositiveButton("♥", new DialogInterface.OnClickListener() {
+                                    public void onClick(DialogInterface dialog, int which) {
+                                        // Do something else
+                                    }
+                                });
+                        AlertDialog alert = builder.create();
+                        alert.show();
+                    }
                 }
-            }
-        });
+            });
+        } else {
+            sammyraBtn.setVisibility(View.INVISIBLE);
+        }
 
         return view;
     }
